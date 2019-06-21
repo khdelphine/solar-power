d8296ba0d75bbf1a2c6876fabdb0322591182b81
Delphine Khanna <khdelphine@users.noreply.github.com>
2019-06-21 09:01:13 -0700

    Improved computation of zonal statistics

--- Solar_Power.py
+++ Solar_Power.py
@@ -19,7 +19,7 @@ import arcpy.sa # Spatial Analyst
 # *********
 # Set up global variables
 base_path = "C:\\Users\\del10314\\Desktop\\Dk_projects\\Solar_power2"
-tiff_home = base_path + "\\New_data"
+tiff_home = base_path + "\\New_data2"
 fc_home = base_path + "\\Solar_power2.gdb"
 
 
@@ -45,17 +45,17 @@ def set_up_env():
 # Select only the buildings whose footprint is large enough
 def find_large_buildings():
     # Select the rooftops that are large enough to be suitable:
-    arcpy.SelectLayerByAttribute_management("Building_Footprints_Clipped", "NEW_SELECTION", "Shape_Area > 40")
+    arcpy.SelectLayerByAttribute_management("Building_Footprints_Clipped", "NEW_SELECTION", "Shape_Area >= 40")
     # Save to a new feature class and do some clean up
-    arcpy.CopyFeatures_management("Building_Footprints_Clipped", "Building_Footprints_Gt40")
-    arcpy.SelectLayerByAttribute_management(tiff_home + "\\Building_Footprints_Clipped", "CLEAR_SELECTION")
+    arcpy.CopyFeatures_management("Building_Footprints_Clipped", fc_home + "\\Building_Footprints_Large")
+    arcpy.SelectLayerByAttribute_management("Building_Footprints_Clipped", "CLEAR_SELECTION")
 
 # Generate a raster showing the average solar radiation only for the building footprints
 def get_SR_for_buildings():
-    outExtractByMask = arcpy.sa.ExtractByMask("Avg_Solar_Radiation_Shifted.tif", "Building_Footprints_Gt40")
-    outExtractByMask.save("Avg_SR_for_buildings2.tif")
+    outExtractByMask = arcpy.sa.ExtractByMask("Avg_Solar_Radiation_Shifted.tif", "Building_Footprints_Large")
+    outExtractByMask.save("Avg_SR_for_buildings.tif")
     # Load theraster into the MXD
-    arcpy.MakeRasterLayer_management("Avg_SR_for_buildings2.tif", "Avg_SR_for_buildings2")
+    arcpy.MakeRasterLayer_management(tiff_home + "\\Avg_SR_for_buildings.tif", "Avg_SR_for_buildings")
 
 
 # Remove the areas where the slope is too high to be suitable for solar panels
@@ -64,7 +64,7 @@ def remove_low_slope():
     outSlope.save(tiff_home + "\\Slope_clipped_shifted.tif")
     arcpy.MakeRasterLayer_management(tiff_home + "\\Slope_clipped_shifted.tif", "Slope_clipped_shifted")
 
-    outCon = arcpy.sa.Con ("Slope_clipped_shifted", "Avg_SR_for_buildings2", "", "VALUE <= 45")
+    outCon = arcpy.sa.Con ("Slope_clipped_shifted", "Avg_SR_for_buildings", "", "VALUE <= 45")
     outCon.save("Avg_SR_for_buildings_low_slope.tif")
     arcpy.MakeRasterLayer_management(tiff_home + "\\Avg_SR_for_buildings_low_slope.tif", "Avg_SR_for_buildings_low_slope")
 
@@ -82,45 +82,53 @@ def find_suitable_areas():
     remove_low_slope()
     remove_low_performance_areas()
 
-
-# Compute the Solar Radiation amounts for each building
-def get_building_level_numbers():
-        # Compute SR stats for each building. The output is a table
-        arcpy.sa.ZonalStatisticsAsTable("Building_Footprints_Gt40", "OBJECTID", "Avg_SR_for_buildings_high_performance",
-                                        fc_home + "\\bldg_stats_table", "DATA", "ALL")
-        arcpy.AddJoin_management("Building_Footprints_Gt40", "OBJECTID", "bldg_stats_table", "OBJECTID_1", "KEEP_ALL")
-        arcpy.CopyFeatures_management("Building_Footprints_Gt40", fc_home + "\\Bldg_with_stats")
-        arcpy.RemoveJoin_management("Building_Footprints_Gt40")
+# Compute the Solar Radiation amount for each "zone"
+def get_stats_per_zone(zone, out):
+        # Compute SR stats for each zone. The output is a table
+        arcpy.sa.ZonalStatisticsAsTable(zone, "OBJECTID", "Avg_SR_for_buildings_high_performance",
+                                        fc_home + "\\" + out + "_table", "DATA", "MEAN")
+        arcpy.AddJoin_management(zone, "OBJECTID", out + "_table", "OBJECTID_1", "KEEP_ALL")
+        arcpy.CopyFeatures_management(zone, fc_home + "\\" + out)
+        arcpy.RemoveJoin_management(zone)
 
 # Remove buildings that have too little surface that is suitable for solar panels
 def remove_low_suitability_bldgs():
     # Select the rooftops that have enough suitability are for solar panels:
-    arcpy.SelectLayerByAttribute_management("Bldg_with_stats", "NEW_SELECTION", "bldg_stats_table_AREA >= 30")
+    arcpy.SelectLayerByAttribute_management("Bldg_with_stats", "NEW_SELECTION", "Bldg_with_stats_table_AREA >= 30")
     # Save to a new feature class and do some clean up
-    arcpy.CopyFeatures_management("Bldg_with_stats", fc_home + "\\suitable_buildings")
+    arcpy.CopyFeatures_management("Bldg_with_stats", fc_home + "\\Suitable_buildings")
     arcpy.SelectLayerByAttribute_management("Bldg_with_stats", "CLEAR_SELECTION")
 
 # Compute the potential electric power production
-def compute_elec_prod_numbers():
-    # Per building:
-    arcpy.AddField_management("suitable_buildings", "EP_per_bldg_Wh_sqm","DOUBLE")
-    formula_expr = "(!bldg_stats_table_MEAN! * 0.15) * 0.86"
-    arcpy.CalculateField_management("suitable_buildings", "EP_per_bldg_Wh_sqm",
-                                    formula_expr)
+def compute_EP_numbers(zone, prefix):
+    # Per "zone":
+    arcpy.AddField_management(zone, "Daily_EP_Wh_per_sqm","DOUBLE")
+    formula_expr = "(!" + prefix + "_with_stats_table_MEAN! * 0.15) * 0.86"
+    arcpy.CalculateField_management(zone, "Daily_EP_Wh_per_sqm", formula_expr)
 
-    # Per building per sq m
-    arcpy.AddField_management("suitable_buildings", "EP_per_bldg_kWh","DOUBLE")
-    formula_expr2 = "(!EP_per_bldg_Wh_sqm! * !bldg_stats_table_AREA!)/ 1000"
-    arcpy.CalculateField_management("suitable_buildings", "EP_per_bldg_kWh",
-                                    formula_expr2)
+    # Per "zone" per sq m
+    arcpy.AddField_management(zone, "Daily_EP_kWh_total","DOUBLE")
+    formula_expr2 = "(!Daily_EP_Wh_per_sqm! * !" + prefix + "_with_stats_table_AREA!)/ 1000"
+    arcpy.CalculateField_management(zone, "Daily_EP_kWh_total", formula_expr2)
+
+
+def stats_per_building():
+    get_stats_per_zone("Building_Footprints_Large", "Bldg_with_stats")
+    remove_low_suitability_bldgs()
+    compute_EP_numbers("Suitable_buildings", "Bldg")
+
+def stats_per_neighborhood():
+    get_stats_per_zone("GloverParkBoundaries", "Neighborhood_with_stats")
+    compute_EP_numbers("Neighborhood_with_stats", "Neighborhood")
+
 
 
 # ***************************************
 # Begin Main
 print_time_stamp("Start")
 set_up_env()
-find_suitable_areas()
-get_building_level_numbers()
-remove_low_suitability_bldgs()
-compute_elec_prod_numbers()
+compute_SR_for_a_day()
+#find_suitable_areas()
+#stats_per_building()
+#stats_per_neighborhood()
 print_time_stamp("Done")
